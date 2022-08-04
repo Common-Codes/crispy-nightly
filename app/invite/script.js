b14a5f067714cac12001212a2a0d21d78ad26bb1
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-08-04 20:05:14 +0200

    Update script.js

--- app/invite/script.js
+++ app/invite/script.js
@@ -2,5 +2,14 @@ window.onload = function(){
     const fragment = new URLSearchParams(window.location.hash.slice(1));
     const [code, joined, expiry] = [fragment.get('code'), fragment.get('meta'), fragment.get('expire')];
 
-    if(code != null)
+    if(code != null){
+        store.collection("guild").where("invite", "==", code)
+        .get()
+        .then((querySnapshot) => {
+            querySnapshot.forEach((doc) => {
+                const guild = doc.data()
+                content.innerHTML = `<p>WIP</p>`
+            });
+        })
+    }
 }
