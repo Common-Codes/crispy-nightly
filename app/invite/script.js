c1be288313250e079d1fee9ee1d50c19884b7f21
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-08-06 18:23:43 +0200

    the collective name is guilds
    also pls work

--- app/invite/script.js
+++ app/invite/script.js
@@ -4,12 +4,13 @@ window.onload = function(){
     const [code, joined, expiry] = [fragment.get('code'), fragment.get('meta'), fragment.get('expire')];
 
     if(code != null){
-        store.collection("guild").where("invite", "==", code)
+        store.collection("guilds").where("invite", "==", code)
         .get()
         .then((querySnapshot) => {
             querySnapshot.forEach((doc) => {
                 const guild = doc.data()
-                content.innerHTML = `<p>${code}</p>`
+                content.innerHTML = `<p>You have been invited to join ${guild.title} on Crispy!</p>`
+                document.body.style.backgroundImage = `url('${guild.banner}')`;
             });
         })
     }
