a0da096c2c0e234e364a1c805ce2c210c1e51726
TTS <90527370+TallerThanShort@users.noreply.github.com>
2022-09-23 18:33:08 +0200

    change invites to queries (?)
    Changed /invite to use ? rather than #

--- app/invite/script.js
+++ app/invite/script.js
@@ -1,6 +1,6 @@
 window.onload = function(){
     const content = document.getElementById("poster")
-    const fragment = new URLSearchParams(window.location.hash.slice(1));
+    const fragment = new URLSearchParams(window.location.search.slice(1));
     const [code, joined, expiry] = [fragment.get('code'), fragment.get('meta'), fragment.get('expire')];
 
     if(code != null){
